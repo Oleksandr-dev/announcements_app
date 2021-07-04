42fdde51ae1c9c5e4ca7ad9e683f6078cfabadff
OleksandrK-dev <ukorkushko@gmail.com>
2021-07-04 13:14:28 +0300

    First commit

---
+++ src/components/pages/AllAnnouncement.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {TemplateClassicPage} from "../templates/templateClassicPage";
+import {LongList} from "../molecules/LongList";
+
+export const AllAnnouncement = () => {
+    return(
+        <div>
+            <TemplateClassicPage/>
+            <LongList/>
+            AllAnnouncement
+        </div>
+    )
+}
